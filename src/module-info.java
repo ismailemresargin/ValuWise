78d3c15d23c7ef370a66a9ef500e8c1c7fe4b97e
Huseyn Alr <alirzayevhuseyn57@gmail.com>
2026-04-26 21:54:49 +0300

    build: Proje altyapısı Maven'a taşındı, kütüphane ve modül sorunları giderildi.

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.valuwise {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.fasterxml.jackson.databind;
-    opens com.example.valuwise to javafx.fxml;
-    exports com.example.valuwise;
-}
