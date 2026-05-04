49fee785c43b672ad14d5aaaa184ae27016271d4
Huseyn Alr <alirzayevhuseyn57@gmail.com>
2026-05-04 12:35:40 +0300

    Kendi yedegim

---
+++ src/com/example/valuwise/HousePricingEngine.java
@@ -0,0 +1,20 @@
+package com.example.valuwise;
+
+public class HousePricingEngine {
+    public double tahminiDegerHesapla(House house) {
+        double tabanFiyat = house.getMetrekare() * 30000;
+
+        if (house.isDenizManzarasiVarMi()) tabanFiyat += 600000;
+        if (house.isOtoparkVarMi()) tabanFiyat += 150000;
+        if (house.isAsansorVarMi()) tabanFiyat += 100000;
+
+        if (house.isDepremeDayanikliMi()) tabanFiyat *= 1.25;
+        else tabanFiyat *= 0.65;
+
+        double yipranmaPayi = (house.getBinaYasi() * 0.01) * tabanFiyat;
+        tabanFiyat -= yipranmaPayi;
+
+        house.setTahminiDeger(tabanFiyat);
+        return tabanFiyat;
+    }
+}
---
+++ src/com/example/valuwise/Launcher.java
@@ -0,0 +1,7 @@
+package com.example.valuwise;
+
+public class Launcher {
+    public static void main(String[] args) {
+        HelloApplication.main(args); // Asıl uygulamayı buradan gizlice tetikliyoruz
+    }
+}
