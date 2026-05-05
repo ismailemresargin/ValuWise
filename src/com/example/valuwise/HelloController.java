04214b6f628b5666356946ef052a2a1d9a860f21
huseynalr <alirzayevhuseyn57@gmail.com>
2026-05-05 17:10:26 +0300

    feat: Konut degerleme altyapisi ve form sifirlama mantigi tamamlandi

--- src/com/example/valuwise/HelloController.java
+++ src/com/example/valuwise/HelloController.java
@@ -10,7 +10,6 @@ public class HelloController {
 
     @FXML private ComboBox<String> cityComboBox;
     @FXML private ComboBox<String> districtComboBox;
-<<<<<<< HEAD
 
     @FXML
     public void initialize() {
@@ -47,31 +46,6 @@ public class HelloController {
     }
 
     @FXML
-=======
-    @FXML
-    public void initialize() {
-        cityComboBox.getItems().addAll(SehirVerileri.getIller());
-        districtComboBox.setDisable(true);
-
-        cityComboBox.setOnAction(event -> {
-            String secilenSehir = cityComboBox.getValue();
-            if (secilenSehir != null) {
-                districtComboBox.getItems().clear();
-
-                //ilce yaziniz i tekrar ekle
-                districtComboBox.getItems().add("İlçe Seçiniz");
-
-                // yeni ile gore ilceleri yukle
-                districtComboBox.getItems().addAll(SehirVerileri.getIlceler(secilenSehir));
-
-                // yazının varsayılan olarak secili gelmesini sağla
-                districtComboBox.getSelectionModel().select(0);
-
-                districtComboBox.setDisable(false);
-            }
-        });
-    }
->>>>>>> origin/huseyn
     protected void onCalculateButtonClick() {
         try {
             House yeniEv = new House(
@@ -91,7 +65,6 @@ public class HelloController {
             houseResultLabel.setText("Hata: Verileri kontrol edin!");
         }
     }
-<<<<<<< HEAD
 
     //formu sifirla butonu
     @FXML
@@ -123,6 +96,4 @@ public class HelloController {
         // imleci en bastaki kutuya yonlendiriyoruz
         ilanBasligiField.requestFocus();
     }
-=======
->>>>>>> origin/huseyn
 }
